6d14bc8900aa688352f250c0b973795b7e6539ed
blckclov3r <blckclov3r@gmail.com>
2022-08-01 01:30:26 +0800

    redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,17 +1,50 @@
 
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore,combineReducers } from '@reduxjs/toolkit';
 import budgetModal from '../feature/budget/modalSlice'
 import budgetList from '../feature/budget/budgetSlice'
 import expenseModal from '../feature/expense/modalSlice'
 import expenseList from '../feature/expense/expenseSlice'
 
 
-export const store = configureStore({
-    reducer: {
+
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+  import storage from 'redux-persist/lib/storage'
+  
+  const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+
+  const rootReducer = combineReducers(
+    {
         budgetModal: budgetModal,
         budgets: budgetList,
-
         expenseModal: expenseModal,
         expenses: expenseList
     }
-})
+  )
+
+  const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+  export const store = configureStore({
+    reducer: persistedReducer,
+    devTools: false,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
+  })
+
+  export const persistor = persistStore(store)
